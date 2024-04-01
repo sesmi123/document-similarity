0dfb6caeab6c6b222036e251d85afe685548cd53
sesmi123 <48611310+sesmi123@users.noreply.github.com>
2024-04-01 20:55:12 +0530

    jaccards similarity

---
+++ document_similarity/jaccard.py
@@ -0,0 +1,27 @@
+
+def jaccard_similarity(doc1, doc2):
+    """
+    Example usage:
+    doc1 = "the house had a tiny little mouse"
+    doc2 = "the cat saw the mouse in the little house"
+    similarity_score = jaccard_similarity(doc1, doc2)
+
+    print(f"Jaccard Similarity: {similarity_score}")
+
+    Disadvantage:
+    As the size of the document increases, the number of common words will increase, 
+    even though the two documents are semantically different.
+    """
+
+    # Tokenization: Split the documents into sets of words using space as delimiter
+    words_doc1 = set(doc1.split())
+    words_doc2 = set(doc2.split())
+    
+    # Find the intersection and union of the two sets
+    intersection = words_doc1.intersection(words_doc2)
+    union = words_doc1.union(words_doc2)
+    
+    # Calculate Jaccard Similarity
+    similarity = len(intersection) / len(union)
+    return similarity
+
