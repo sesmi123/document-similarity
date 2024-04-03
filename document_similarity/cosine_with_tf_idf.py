dfbf4d5bd746fe978b6067e8b141dbe26abf81c6
sesmi123 <48611310+sesmi123@users.noreply.github.com>
2024-04-03 21:05:50 +0530

    refactor code

--- document_similarity/cosine_with_tf_idf.py
+++ document_similarity/cosine_with_tf_idf.py
@@ -1,21 +1,26 @@
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 from preprocessing import preprocess
+
+# Vectorization - term frequency-inverse document frequency
+vectorizer = TfidfVectorizer()
+
+def cosine_similarity_with_tf_idf(docA, docB):
+    
+    docA_clean = preprocess(docA)
+    docB_clean = preprocess(docB)
+
+    tfidf_vectors = vectorizer.fit_transform([docA_clean, docB_clean])
+
+    # Calculate Cosine Similarity
+    cosine_sim = cosine_similarity(tfidf_vectors[0:1], tfidf_vectors)
+    print(f"Cosine Similarity: {cosine_sim}")
+
+    return cosine_sim
 
 
 # Sample documents
 docA = "The sky is blue."
 docB = "The sun is bright."
 
-docA_clean = preprocess(docA)
-docB_clean = preprocess(docB)
-
-
-# Vectorization - term frequency-inverse document frequency
-vectorizer = TfidfVectorizer()
-tfidf_vectors = vectorizer.fit_transform([docA_clean, docB_clean])
-
-from sklearn.metrics.pairwise import cosine_similarity
-
-# Calculate Cosine Similarity
-cosine_sim = cosine_similarity(tfidf_vectors[0:1], tfidf_vectors)
-print(f"Cosine Similarity: {cosine_sim}")
+cosine_similarity_with_tf_idf(docA, docB)
--- document_similarity/euclidean.py
+++ document_similarity/euclidean.py
@@ -1,22 +1,27 @@
+from scipy.spatial import distance
 from preprocessing import preprocess
+
+# Vectorization - Bag of Words
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer()
+
+
+def euclidean_similarity(docA, docB):
+    
+    docA_clean = preprocess(docA)
+    docB_clean = preprocess(docB)
+
+    vectors = vectorizer.fit_transform([docA_clean, docB_clean]).toarray()
+
+    euclidean_dist = distance.euclidean(vectors[0], vectors[1])
+    print(f"Euclidean Distance between Document A and B: {euclidean_dist}")
+
+    return euclidean_dist
+
 
 # Sample documents
 docA = "The sky is blue."
 docB = "The sun is bright."
 
-docA_clean = preprocess(docA)
-docB_clean = preprocess(docB)
+euclidean_similarity(docA, docB)
 
-
-# Vectorization - Bag of Words
-from sklearn.feature_extraction.text import CountVectorizer
-
-vectorizer = CountVectorizer()
-# Fit and transform the documents
-vectors = vectorizer.fit_transform([docA_clean, docB_clean]).toarray()
-
-# Calculating Euclidean Distance
-from scipy.spatial import distance
-
-euclidean_dist = distance.euclidean(vectors[0], vectors[1])
-print(f"Euclidean Distance between Document A and B: {euclidean_dist}")
